eda5983a2a437d02b82e26e631ebfa5b64d34951
culver <culv@users.noreply.github.com>
2018-06-25 11:37:32 -0400

    fix images

--- main.py
+++ main.py
@@ -184,6 +184,12 @@ def train(model):
 					# Get reconstructed images as grid (must detach from Pytorch Variable first)
 					recons_grid = utils.batch_to_grid(recons.detach())
 
+					print(ground_truth_grid.shape)
+					print(separator.shape)
+					print(recons_grid.shape)
+
+					sys.exit()
+
 					# Stack ground truth images, separator, and reconstructions into 1 image
 					image = np.concatenate((ground_truth_grid, separator, recons_grid), 1)
 
